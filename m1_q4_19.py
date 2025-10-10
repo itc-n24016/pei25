f2f417d9fbde7ad6db345ffe6628bc9796ca6a6b
itc-n24016 <n24016@std.it-college.ac.jp>
2025-10-10 02:32:09 +0000

    1010問4

---
+++ m1_q4_19.py
@@ -0,0 +1,14 @@
+a = [1, 2, 3, 4]
+id_a = id(a)
+b = a.copy()
+id_b = id(b)
+
+if id_a == id_b:
+	result = 'A'
+elif id(a) == id(b):
+	result = 'B'
+elif id_a == id_b:
+	result = 'C'
+else:
+	result = 'D'
+print(result)
