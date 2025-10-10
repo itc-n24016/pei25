0bee723f110636f80eac61a42f179cd4bf0425db
itc-n24016 <n24016@std.it-college.ac.jp>
2025-10-10 02:54:35 +0000

    リストを一つの文字列にする

---
+++ m1_q4_18_kadai.py
@@ -0,0 +1,10 @@
+phrase = 'PythonProgramming'
+list_p = []#空リストの作成
+
+for p in phrase:
+    if  p not in list_p:
+        list_p.append(p)#list_pに同じ文字がなければ追加する関数
+print(list_p)
+print(len(phrase), len(list_p))
+print(len(phrase) - len(list_p))
+print("".join(list_p))
