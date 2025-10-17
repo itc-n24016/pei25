d4493b6df255adc2e7347ba3db139c640c5b3fb6
itc-n24016 <n24016@std.it-college.ac.jp>
2025-10-17 02:44:14 +0000

    1017 問4 組み込み関数

---
+++ m1_q4_22.py
@@ -0,0 +1,9 @@
+a = 17
+b = 5
+dm = divmod(a,b) # 商の余りをタプルで返す
+p = pow(dm[0], dm[1]) # 3 ** 2 = 9
+all_num = [a, b, dm[0], dm[1], p] # [17,5,3,2,9]
+min_all = min(all_num) # 2
+max_all = max(all_num) # 17
+sum_all = sum(all_num) # 36
+print(f'{min_all}, {max_all}, {sum_all}')
