0a8f83b97fd1edd5b134ef8961d1a8e57275398c
itc-n24016 <n24016@std.it-college.ac.jp>
2025-10-24 01:15:43 +0000

    1024 問4 文字のカウント

---
+++ m1_q4_24.py
@@ -0,0 +1,15 @@
+animals = ['Dog', 'Cat', 'Rabbit', 'Horse', 'Dolphin']
+total = 0
+
+for animal in animals:
+    from_D = animal.startswith('D') # animalが'D'で始まる場合true、それ以外をfalseを返す
+    is_long = len(animal) > 5
+    if from_D and is_long:
+        break
+    elif not from_D and is_long:
+        total += animal.find('b')
+        print(total)
+    else:
+        total += len(animal)
+        print(total)
+print(total)
