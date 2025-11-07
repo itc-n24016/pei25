e3ec7caadb346a47d98a31f7582272369a1fd735
itc-n24016 <n24016@std.it-college.ac.jp>
2025-11-07 02:58:34 +0000

    問題2

---
+++ m2/m2_q3.py
@@ -0,0 +1,16 @@
+list_a = [7.0 , 9.0 , 11 , 14.0 , 3 , 4.0 , 2 ]
+list_b = [12, 1, 6.0, 3, 7]
+
+print(list_a[1] * list_b[-2])
+print(str(list_a[4] + int(list_b[2])) + '番')
+print(divmod(list_a[3], list_b[4]))
+
+list_c = [2.05, 5]
+list_b += list_c
+
+print(list_a[2] % list_b[4])
+print(min(list_b[2:6]))
+print(round(list_c[0], list_b[1]))
+
+print(list_b)
+print(list_b[2:6])
---
+++ m2/m2_q4.py
@@ -0,0 +1,10 @@
+a = 0b10111
+b = 0b11101
+c = a | b
+print(c)
+
+
+d = 6
+e = pow(d, 3)
+f = divmod(e, d)
+print(f)
