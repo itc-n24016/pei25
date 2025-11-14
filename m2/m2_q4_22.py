1af86458ba2d91fe85fb4317249eebc62899259a
itc-n24016 <n24016@std.it-college.ac.jp>
2025-11-14 03:10:54 +0000

    1114 問4 リストとappend

---
+++ m2/m2_q4_22.py
@@ -0,0 +1,15 @@
+list_a = ['Yokohama', 'Osaka', 'Nagoya', 'Sapporo']
+list_b = []
+list_c = []
+list_d = []
+
+for name in list_a:
+    if len(name) > 6:
+        list_b.append(name)
+    elif name.count('a') > 1:
+        list_c.append(name)
+    else:
+        list_d.append(name)
+print(list_b)
+print(list_c)
+print(list_d)
