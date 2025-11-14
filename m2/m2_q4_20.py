c62308b5ba2e87dd6e5dbfc3aa9dc09604c53fb7
itc-n24016 <n24016@std.it-college.ac.jp>
2025-11-14 01:56:49 +0000

    1114 問4 rangeto 関数 \
    :

---
+++ m2/m2_q4_20.py
@@ -0,0 +1,10 @@
+num = 3
+list_a = []
+while num < 5 :
+    for i in range(num, 10, 3): # i から 10 未満までを 3 ずつ増やしながらループする
+        print(list_a)
+        i -= 6
+        list_a.append(i)
+    num += 1
+print(sum(list_a))
+print(list_a)
